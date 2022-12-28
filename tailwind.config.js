04390e28e467e4b4405513da93f1ef916ff2f3ac
l-marcel <lmgh1312@gmail.com>
2022-12-28 06:27:58 -0300

    fix: removing error in the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "roboto": ["Roboto", "sans-serif"]
+        "roboto": ["var(--roboto-font)", "sans-serif"]
       },
       colors: {
         white: "#fff",
